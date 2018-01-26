32af3e2e8dca94cf86d1230ceb886cc102e45952
Nguyen Minh Tri <tringuyen.moa@gmail.com>
2018-01-26 11:07:51 +0700

    da chinh sua xong

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Ngan = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -44,23 +46,43 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 43);
+            this.textBox1.Location = new System.Drawing.Point(40, 43);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(120, 69);
+            this.textBox2.Location = new System.Drawing.Point(57, 69);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(163, 20);
             this.textBox2.TabIndex = 1;
+            // 
+            // Ngan
+            // 
+            this.Ngan.Location = new System.Drawing.Point(57, 161);
+            this.Ngan.Name = "Ngan";
+            this.Ngan.Size = new System.Drawing.Size(75, 23);
+            this.Ngan.TabIndex = 2;
+            this.Ngan.Text = "Ngan";
+            this.Ngan.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Ngan);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -76,6 +98,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Ngan;
+        private System.Windows.Forms.Button button2;
     }
 }
 
